89cf6b219839b8768d8bcd1c03fbc8505dd04dc8
Jerin Philip <jerin.philip@students.iiit.ac.in>
2020-08-20 21:10:18 +0530

    Can't unpickle need through nltk

--- sentence_tokenizers/test.py
+++ sentence_tokenizers/test.py
@@ -1,17 +1,16 @@
 from argparse import ArgumentParser
-from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 import nltk
+from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 import pickle
 
 def test(test_corpus_path, model):
-    with open(test_corpus_path, 'rb') as save_file:
-        tokenizer = pickle.load(save_file)
-        with open(test_corpus_path) as test_file:
-            contents = test_file.read()
-            contents = contents.replace('\n', ' ')
-            sentences = tokenizer.tokenize(contents)
-            for sentence in sentences:
-                print(sentence)
+    tokenizer = nltk.data.load('file:'+model)
+    with open(test_corpus_path) as test_file:
+        contents = test_file.read()
+        contents = contents.replace('\n', ' ')
+        sentences = tokenizer.tokenize(contents)
+        for sentence in sentences:
+            print(sentence)
 
 
 
