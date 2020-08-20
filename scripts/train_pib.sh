ce2d264618cbb0978d87654b4225b23d5d618246
Jerin Philip <jerin.philip@students.iiit.ac.in>
2020-08-20 22:55:30 +0530

    Adding sample script which generated things on node5

---
+++ scripts/train_pib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+for LLANG in hi mr bn en ml te ta ur or pa gu
+do
+    python3 -m sentence_tokenizers.trainer --train-corpus ../monolingual-exports-pib/raw.$LLANG --save-model trained-models/$LLANG.pib.pickle
+done; 
