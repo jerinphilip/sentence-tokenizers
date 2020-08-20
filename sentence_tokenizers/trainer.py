6f6eb168a6d118d03f06763e7d276461ede01cfb
Jerin Philip <jerin.philip@students.iiit.ac.in>
2020-08-20 20:28:07 +0530

    bringing ml tokenizer trained on newspaper in

--- sentence_tokenizers/trainer.py
+++ sentence_tokenizers/trainer.py
@@ -1,24 +1,23 @@
 from argparse import ArgumentParser
 from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 import nltk
+import pickle
 
-def train(corpus):
+def train(corpus, save_path):
     with open(corpus) as fp:
         contents = fp.read()
         # print(contents)
         tokenizer = PunktSentenceTokenizer()
-        tokenizer = nltk.data.load('tokenizers/punkt/malayalam.pickle')
-        # contents = contents.replace('\n', ' ')
-        #params = tokenizer.train(contents, verbose=True)
-        print(tokenizer.tokenize("Hello world. This is a car, Dr. Einstein. BUt does it work very well, Mr. Hyde?"))
-        keys = ['abbrev_types', 'collocations', 'sent_starters']
-        for key in keys:
-            print(key, getattr(params, key))
+        ## contents = contents.replace('\n', ' ')
+        params = tokenizer.train(contents, verbose=True)
+        # print(tokenizer.tokenize("Hello world. This is a car, Dr. Einstein. But does it work very well, Mr. Hyde?"))
+        with open(save_path, 'wb+') as save_file:
+            pickle.dump(tokenizer, save_file)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--train-corpus', type=str, required=True)
     parser.add_argument('--save-model', type=str, required=True)
     args = parser.parse_args()
-    train(args.train_corpus)
+    train(args.train_corpus, args.save_model)
 
