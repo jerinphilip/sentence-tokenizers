892da1a209382ccae143cd01d448980e8b02bd73
Jerin Philip <jerin.philip@students.iiit.ac.in>
2020-08-20 20:57:00 +0530

    Working example?

--- sentence_tokenizers/inspection.py
+++ sentence_tokenizers/inspection.py
@@ -3,13 +3,16 @@ import nltk
 from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 import pickle
 
-def _inspect(save_path):
-    formatted_path = 'file:{}'.format(save_path)
-    tokenizer = nltk.data.load(formatted_path)
+def inspect_tokenizer(tokenizer):
     param_keys = ['abbrev_types', 'collocations', 'sent_starters']
     for key in param_keys:
         print(key, getattr(tokenizer._params, key))
-       t print()
+        print()
+
+def _inspect(save_path):
+    formatted_path = 'file:{}'.format(save_path)
+    tokenizer = nltk.data.load(formatted_path)
+    inspect_tokenizer
 
 if __name__ == '__main__':
     parser = ArgumentParser()
--- sentence_tokenizers/trainer.py
+++ sentence_tokenizers/trainer.py
@@ -2,17 +2,23 @@ from argparse import ArgumentParser
 from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 import nltk
 import pickle
+from .inspection import inspect_tokenizer
 
 def train(corpus, save_path):
+    # Refer:
+    # https://github.com/alvations/DLTK/blob/84bb7daeda21c18424518731928aea103c15caa1/dltk/tokenize/tokenizer.py#L37
+
     with open(corpus) as fp:
+        punkt = PunktTrainer()
         contents = fp.read()
-        # print(contents)
-        tokenizer = PunktSentenceTokenizer()
-        ## contents = contents.replace('\n', ' ')
-        params = tokenizer.train(contents, verbose=True)
-        # print(tokenizer.tokenize("Hello world. This is a car, Dr. Einstein. But does it work very well, Mr. Hyde?"))
+        punkt.train(contents, verbose=True, finalize=True)
+        punkt.finalize_training(verbose=True)
+        model = PunktSentenceTokenizer(punkt.get_params())
+
         with open(save_path, 'wb+') as save_file:
-            pickle.dump(tokenizer, save_file)
+            pickle.dump(model, save_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+        inspect_tokenizer(model)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
